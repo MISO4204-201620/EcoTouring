543d582d2c64118317d9035f926818a4f227c090
Gloria <Gloria@HostA>
2013-04-16 21:39:10 -0500

    Suprimi el System

--- src/java/co/edu/uniandes/integrandes/fachada/Controlador.java
+++ src/java/co/edu/uniandes/integrandes/fachada/Controlador.java
@@ -43,7 +43,6 @@ public class Controlador {
          
           public String actualizarRecurso()
         {
-            System.out.println("Un cambio adicional"); 
             return "ir";             
         }
          //Llamadas
